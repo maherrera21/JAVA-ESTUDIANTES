81757e99b1f5cf35881b32ace929617a456c3c04
miguelithoh <maherrera21@utpl.edu.ec>
2013-07-15 00:13:09 -0500

    Create Estudiantes.java

---
+++ Estudiantes.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class Estudiantes {
+
+    String nombre;
+    String apellido;
+    String dir;
+    String lugNac;
+    int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+    public String getLugNac() {
+        return lugNac;
+    }
+
+    public void setLugNac(String lugNac) {
+        this.lugNac = lugNac;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
